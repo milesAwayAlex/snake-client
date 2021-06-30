ec642097f1942283d3b2d917da36ca4b5df57743
Alex M <milesAwayAlex@gmail.com>
2021-06-30 13:00:31 -0700

    Implement basic movement commands

--- input.mjs
+++ input.mjs
@@ -1,12 +1,13 @@
-// conn.on('connect', () => conn.write('Move: up'));
-
 /*
 "Move: up" - move up one square (unless facing down)
 "Move: down" - move down one square (unless facing up)
 "Move: left" - move left one square (unless facing right)
 "Move: right" - move left one square (unless facing left)
 */
-const setupInput = () => {
+let conn;
+
+const setupInput = connection => {
+  conn = connection;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -16,10 +17,11 @@ const setupInput = () => {
 };
 
 const handleInput = key => {
-  if (key === '\u0003') {
-    process.stdout.write('laika come home\n');
-    process.exit();
-  }
+  if (key === '\u0003') process.exit();
+  else if (key === 't') conn.write('Move: up');
+  else if (key === 's') conn.write('Move: down');
+  else if (key === 'h') conn.write('Move: left');
+  else if (key === 'n') conn.write('Move: right');
 };
 
 export default setupInput;
--- play.mjs
+++ play.mjs
@@ -2,9 +2,12 @@ import connect from './client.mjs';
 import setupInput from './input.mjs';
 
 console.log('Connecting ...');
-const conn = connect(process.argv[2] || 'localhost', process.argv[3] || 50541);
+const conn = connect(
+  process.argv[2] || '135.23.223.133',
+  process.argv[3] || 50542
+);
 conn.on('connect', () => {
   console.log('\nSucessfully connected!');
   conn.write('Name: AX');
 });
-setupInput();
+setupInput(conn);
