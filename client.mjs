9b136168a45fc29910b173636cc875186c48aed0
Alex M <milesAwayAlex@gmail.com>
2021-06-29 23:40:18 -0700

    Add client.mjs, mv play.js -> play.mjs

--- client.mjs
+++ client.mjs
@@ -1,19 +1,18 @@
-const net = require('net');
+import {createConnection} from 'net';
 
 // establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
+const connect = () => {
+  const conn = createConnection({
     // host: // IP address here,
     // port: // PORT number here,
   });
-
   // interpret incoming data as text
   conn.setEncoding('utf8');
+
   conn.on('connect', (err, data) => console.log('err:', err, 'data', data));
   conn.on('data', (err, data) => console.log('err:', err, 'data', data));
 
   return conn;
 };
 
-console.log('Connecting ...');
-connect();
+export default connect;
---
+++ play.mjs
@@ -0,0 +1,4 @@
+import connect from './client.mjs';
+
+console.log('Connecting ...');
+connect();
