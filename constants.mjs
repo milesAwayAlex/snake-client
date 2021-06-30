d44dde3457741ef71d91a7d0cc8f4a0a696cc393
Alex M <milesAwayAlex@gmail.com>
2021-06-30 13:32:14 -0700

    Implement constants import

---
+++ constants.mjs
@@ -0,0 +1,4 @@
+const HOST = '135.23.223.133';
+const PORT = 50542;
+
+export {HOST, PORT};
--- play.mjs
+++ play.mjs
@@ -1,11 +1,9 @@
 import connect from './client.mjs';
 import setupInput from './input.mjs';
+import {HOST, PORT} from './constants.mjs';
 
 console.log('Connecting ...');
-const conn = connect(
-  process.argv[2] || '135.23.223.133',
-  process.argv[3] || 50542
-);
+const conn = connect(process.argv[2] || HOST, process.argv[3] || PORT);
 conn.on('connect', () => {
   console.log('\nSucessfully connected!');
   conn.write('Name: AX');
