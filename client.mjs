80df1f2189d38ab8ded4d5d73ecd2f45c6ec226d
Alex M <milesAwayAlex@gmail.com>
2021-06-30 00:20:56 -0700

    Separate the connection and the client logic

--- client.mjs
+++ client.mjs
@@ -3,18 +3,11 @@ import {createConnection} from 'net';
 // establishes a connection with the game server
 const connect = () => {
   const conn = createConnection({
-    // host: // IP address here,
-    // port: // PORT number here,
+    host: 'localhost', // IP address here,
+    port: 50541, // PORT number here,
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
-
-  conn.on('connect', (err, data) => {
-    console.log('err:', err, 'data:', data, '\nSucessfully connected!');
-    conn.write('Name: AX');
-  });
-  conn.on('data', data => console.log('data:', data));
-
   return conn;
 };
 
--- play.mjs
+++ play.mjs
@@ -1,4 +1,17 @@
 import connect from './client.mjs';
 
 console.log('Connecting ...');
-connect();
+const conn = connect();
+conn.on('connect', () => {
+  console.log('\nSucessfully connected!');
+  conn.write('Name: AX');
+});
+conn.on('data', data => console.log('\nserver:', data));
+// conn.on('connect', () => conn.write('Move: up'));
+
+/*
+"Move: up" - move up one square (unless facing down)
+"Move: down" - move down one square (unless facing up)
+"Move: left" - move left one square (unless facing right)
+"Move: right" - move left one square (unless facing left)
+*/
